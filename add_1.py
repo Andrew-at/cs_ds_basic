008fffd0a745df875d9e1bd56331934694eeb049
A <lloyd.a.higgins@gmail.com>
2025-05-03 19:32:45 -0700

    updates, moved to add_2.py

--- add_1.py
+++ add_1.py
@@ -1,5 +1,7 @@
 import time
 
+#adds each number incrementally
+#prints total
 def add_up_to(num):
 
     total = 0
@@ -8,11 +10,12 @@ def add_up_to(num):
         total += i
     return total
 
+#counting milliseconds
 start_time = time.perf_counter()
 end_time = time.perf_counter()
 elapsed_time = (end_time - start_time) * 1000.0
 
-num1 = add_up_to(1000)
+num1 = add_up_to(5)
 
 print(num1)
 print(elapsed_time / 1000)
--- add_2.py
+++ add_2.py
@@ -1,13 +1,16 @@
 import time
 
+#adds each number incrementally
+#prints total
 def add_up_to_2(num):
     return num * (num +1) /2
 
+#counting milliseconds
 start_time = time.perf_counter()
 end_time = time.perf_counter()
 elapsed_time = (end_time - start_time) * 1000.0
 
-num1 = add_up_to_2(100000000)
+num1 = add_up_to_2(5)
 
 print(num1)
 print(elapsed_time / 1000)
